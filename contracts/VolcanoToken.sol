c0d5014259be475c4bc058cd669cb44716fc94bc
george-shammar <george.gbenle@outlook.com>
2021-09-07 12:23:50 +0000

    Implement mintToken

--- contracts/VolcanoToken.sol
+++ contracts/VolcanoToken.sol
@@ -6,5 +6,19 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VolcanoToken is ERC721, Ownable {
+     
+    uint256 tokenId = 0;
+    
+    struct TokenMetaData {
+        uint256 tokenId;
+        uint timestamp;
+        string tokenURI;
+    }
+    
+    mapping(address => TokenMetaData[]) public tokenOwnership;
+    
+    constructor() ERC721("VolcanoToken", "VKT") {
+        
+    }
     
 }
