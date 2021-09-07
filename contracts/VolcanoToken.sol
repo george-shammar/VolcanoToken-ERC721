6b7f80e07ab5b77fa57c5a8b43687bd256ff14d2
george-shammar <george.gbenle@outlook.com>
2021-09-07 11:45:49 +0000

    Import openzeppelin libraries

--- contracts/VolcanoToken.sol
+++ contracts/VolcanoToken.sol
@@ -1,1 +1,6 @@
 // SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
