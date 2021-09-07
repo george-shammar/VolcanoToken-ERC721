3c8e05f490e5a1f2e090461b238ee9998c39d2f2
george-shammar <george.gbenle@outlook.com>
2021-09-07 11:47:48 +0000

    Initiate npm

--- contracts/VolcanoToken.sol
+++ contracts/VolcanoToken.sol
@@ -4,3 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VolcanoToken is ERC721, Ownable {
+    
+}
